207ae517f3b281f4287f5be6f4032a935c057618
sandrasofiaa <117361681+sandrasofiaa@users.noreply.github.com>
2024-01-08 17:33:04 +0100

    added model

---
+++ Domain/Models/ShoppingItemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models
+{
+    public class ShoppingItemModel
+    {
+        public Guid ShoppingItemId { get; set; }
+        public required string ShoppingItemName { get; set; }
+        public int Quantity { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
