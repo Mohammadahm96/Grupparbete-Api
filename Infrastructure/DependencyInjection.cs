18e2aa88a0f4bdd882959c6d5e1c0dd3f023ea80
Mohamad Ahmad <mohamad.ahmad@student.nbi-handelsakademin.se>
2024-01-12 13:17:23 +0100

    Create and registered database

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,23 +1,28 @@
 ﻿using Infrastructure.Authentication;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddSingleton<JWTtokenGenerator>();
 
-            // add services for other Interfaces and responsive Repositories
-
-            // services.AddScoped<IUserRepository, UserRepository>();
+            // Add services for other Interfaces and responsive Repositories
+            // services.AddScoped<IUserRepository, UserRepository>;
 
             // Add service for DbContext
-
-
+            services.AddDbContext<MyDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
 
             return services;
         }
     }
 }
+
