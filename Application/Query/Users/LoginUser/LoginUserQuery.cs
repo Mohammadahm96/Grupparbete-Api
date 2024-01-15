6af10221a867c111a523a8c6a8ce03cf33901312
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-01-15 11:48:59 +0100

    LoginUserQuery and LoginUserQueryHandler created and implemented

---
+++ Application/Query/Users/LoginUser/LoginUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Dto;
+using MediatR;
+
+namespace Application.Query.Users.LoginUser
+{
+    public class LoginUserQuery : IRequest<string>
+    {
+        public UserDto LoginUser { get; }
+        public LoginUserQuery(UserDto loginUser)
+        {
+            LoginUser = loginUser;
+        }
+    }
+}
---
+++ Application/Query/Users/LoginUser/LoginUserQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Elasticsearch.Net;
+using Infrastructure.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Query.Users.LoginUser
+{
+    public class LoginUserQueryHandler : IRequestHandler<LoginUserQuery, string>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public LoginUserQueryHandler(IUserRepository userRepository) 
+        {
+          _userRepository = userRepository;
+        }
+
+        public async Task<string> Handle(LoginUserQuery request, CancellationToken cancellationToken)
+        {
+            string jwtToken = await _userRepository.GetsTokenToLogin(request.LoginUser.UserName, request.LoginUser.Password);
+            if (jwtToken == null)
+            {
+                return null!;
+            }
+            return jwtToken;
+        }
+    }
+}
