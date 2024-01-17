fc296edf3bc5f9d0a193f3c2ba9f8359a1ccc333
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-01-17 13:23:06 +0100

    Commands/Users Register,Update,Delete are finished, Next to do is to create User Controler

---
+++ Application/Commands/Users/RegisterNewUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Models.UserModel;
+using Infrastructure.Interfaces;
+using MediatR;
+
+namespace Application.Commands.Users.RegisterNewUser
+{
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, User>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public RegisterUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<User> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            if (request.NewUser == null || string.IsNullOrEmpty(request.NewUser.UserName) || string.IsNullOrEmpty(request.NewUser.Password))
+            {
+                throw new ArgumentNullException("Invalid user data. Username and password are required.");
+            }
+
+            try
+            {
+                User userToCreate = new User
+                {
+                    UserId = Guid.NewGuid(),
+                    Username = request.NewUser.UserName,
+                    Password = BCrypt.Net.BCrypt.HashPassword(request.NewUser.Password),
+                };
+
+                var createdUser = await _userRepository.RegisterUser(userToCreate);
+
+                return createdUser;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it according to your application's needs
+                throw new ApplicationException("User registration failed.", ex);
+            }
+        }
+    }
+}
---
+++ Application/Commands/Users/UpdateUser/UpdateUserByIdCommand.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dto;
+using Domain.Models.UserModel;
+using MediatR;
+
+namespace Application.Commands.Users.UpdateUser
+{
+    public class UpdateUserByIdCommand : IRequest<User>
+    {
+        public Guid Id { get; }
+        public UserDto UpdatedUser { get; }
+        public UpdateUserByIdCommand(Guid id, UserDto updatedUser)
+        {
+            Id = id;
+            UpdatedUser = updatedUser;
+        }
+    }
+}
