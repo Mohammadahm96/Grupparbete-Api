ee8e9219757038dd25bdb9a8a400dc7b49d51a5d
sandrasofiaa <117361681+sandrasofiaa@users.noreply.github.com>
2024-01-17 09:20:46 +0100

    Added FamilyArticleList to DbContext

--- Infrastructure/Data/DbContext.cs
+++ Infrastructure/Data/DbContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Domain.Models.UserModel;
+using Domain.Models.ListModels;
 
 namespace Infrastructure.Data
 {
     public class MyDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+
+        public DbSet<FamilyArticleList> FamilyArticleList { get; set; }
 
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
         {
