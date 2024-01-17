8a0e6cfb0fd010e16e0ea1b9efc17882042ff207
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-01-18 00:24:26 +0100

    All UserTests are finished and tested

---
+++ Test/UserTests/QueryTest/LoginUserTest.cs
@@ -0,0 +1,55 @@
+﻿using Application.Dto;
+using Application.Query.Users.LoginUser;
+using FakeItEasy;
+using Infrastructure.Interfaces;
+
+namespace Test.UserTests.QueryTest
+{
+    [TestFixture]
+    public class LoginUserTest
+    {
+        [Test]
+        public async Task Handle_ValidUser_ReturnsToken()
+        {
+            // Arrange
+            var fakeUserRepository = A.Fake<IUserRepository>();
+            var loginQuery = new LoginUserQuery(new UserDto { UserName = "username", Password = "password" });
+            var expectedToken = "fakeToken";
+
+            // Mock the behavior of the repository method to return a token
+            A.CallTo(() => fakeUserRepository.GetsTokenToLogin(A<string>._, A<string>._))
+                .WithAnyArguments()
+                .Returns(expectedToken);
+
+            var handler = new LoginUserQueryHandler(fakeUserRepository);
+
+            // Act
+            var result = await handler.Handle(loginQuery, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedToken));
+        }
+
+        [Test]
+        public async Task Handle_InvalidUser_ReturnsNull()
+        {
+            // Arrange
+            var fakeUserRepository = A.Fake<IUserRepository>();
+            var loginQuery = new LoginUserQuery(new UserDto { UserName = "invalid_username", Password = "invalid_password" });
+
+            // Mock the behavior of the repository method to return null for invalid user
+            A.CallTo(() => fakeUserRepository.GetsTokenToLogin(A<string>._, A<string>._))
+                .WithAnyArguments()
+                .Returns(await Task.FromResult<string>(result: null!)); // Return null for an invalid user
+
+            var handler = new LoginUserQueryHandler(fakeUserRepository); // Replace with your actual class name
+
+            // Act
+            var result = await handler.Handle(loginQuery, CancellationToken.None);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
+
