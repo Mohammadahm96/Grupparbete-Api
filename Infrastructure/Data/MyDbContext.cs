8b3f0a377b85af240511978e1822c6af4b5fd7f7
sandrasofiaa <117361681+sandrasofiaa@users.noreply.github.com>
2024-01-17 11:20:56 +0100

    fixed Mydbcontext

--- Infrastructure/Data/MyDbContext.cs
+++ Infrastructure/Data/MyDbContext.cs
@@ -2,16 +2,16 @@
 using Domain.Models.UserModel;
 using Domain.Models.ListModels;
 
-namespace Infrastructure.Data
+namespace Infrastructure.Data;
+
+public class MyDbContext : DbContext
 {
-    public class MyDbContext : DbContext
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<FamilyArticleList> FamilyArticleList { get; set; }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
     {
-        public DbSet<User> Users { get; set; }
-
-        public DbSet<FamilyArticleList> FamilyArticleList { get; set; }
-
-        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
-        {
-        }
     }
+
 }
