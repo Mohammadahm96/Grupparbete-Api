a0ef0b2bade026eb81caad92b8ac9271db342529
sandrasofiaa <117361681+sandrasofiaa@users.noreply.github.com>
2024-01-18 23:13:57 +0100

    Added new repository function to link Id with name

--- Application/Commands/FamilyShoppingListCommands/AddFamilyShoppingList/AddArticleToFamilyShoppingListCommandHandler.cs
+++ Application/Commands/FamilyShoppingListCommands/AddFamilyShoppingList/AddArticleToFamilyShoppingListCommandHandler.cs
@@ -3,43 +3,44 @@ using Domain.Models.ListModels;
 using Infrastructure.Interface;
 using MediatR;
 
-namespace Application.Commands.FamilyShoppingList.AddFamilyShoppingList
+public class AddArticleToFamilyShoppingListCommandHandler : IRequestHandler<AddArticleToFamilyShoppingListCommand, FamilyArticleList>
 {
-    public class AddArticleToFamilyShoppingListCommandHandler : IRequestHandler<AddArticleToFamilyShoppingListCommand, FamilyArticleList>
-    {
-        private readonly IArticleRepository _ArticleRepository;
+    private readonly IArticleRepository _articleRepository;
 
-        public AddArticleToFamilyShoppingListCommandHandler(IArticleRepository articleRepository)
+    public AddArticleToFamilyShoppingListCommandHandler(IArticleRepository articleRepository)
+    {
+        _articleRepository = articleRepository;
+    }
+
+    public async Task<FamilyArticleList> Handle(AddArticleToFamilyShoppingListCommand request, CancellationToken cancellationToken)
+    {
+        // Kontrollera om FamilyId är giltigt
+        if (request.NewFamilyShoppingList.FamilyId == Guid.Empty)
         {
-            _ArticleRepository = articleRepository;
+            throw new Exception("FamilyId is required to add an article to the family shopping list.");
         }
 
-        public async Task<FamilyArticleList> Handle(AddArticleToFamilyShoppingListCommand request, CancellationToken cancellationToken)
+        // Fetch the familyName based on FamilyId (assuming you have a method to get familyName from FamilyId)
+        string familyName = await _articleRepository.GetFamilyNameAsync(request.NewFamilyShoppingList.FamilyId);
+
+        FamilyArticleList familyListToCreate = new FamilyArticleList
         {
-            // Kontrollera om FamilyId är giltigt
-            if (request.NewFamilyShoppingList.FamilyId == Guid.Empty)
-            {
-                throw new Exception("FamilyId is required to add an article to the family shopping list.");
-            }
+            FamilyId = request.NewFamilyShoppingList.FamilyId,
+            ArticleId = Guid.NewGuid(), // This should be the same for a given family
+            ArticleName = request.NewFamilyShoppingList.ArticleName,
+            ArticleQuantity = 1,
+            IsAvailable = true,
+            FamilyName = familyName // Set the familyName
+        };
 
-            FamilyArticleList FamilyListToCreate = new FamilyArticleList
-            {
-                FamilyId = request.NewFamilyShoppingList.FamilyId,
-                ArticleId = Guid.NewGuid(),
-                ArticleName = request.NewFamilyShoppingList.ArticleName,
-                ArticleQuantity = 1,
-                IsAvailable = true
-            };
-
-            try
-            {
-                await _ArticleRepository.AddShoppingListAsync(FamilyListToCreate);
-                return FamilyListToCreate;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Failed to add FamilyList to the database", ex);
-            }
+        try
+        {
+            await _articleRepository.AddShoppingListAsync(familyListToCreate);
+            return familyListToCreate;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Failed to add FamilyList to the database", ex);
         }
     }
 }
--- Infrastructure/Interfaces/IArticleRepository.cs
+++ Infrastructure/Interfaces/IArticleRepository.cs
@@ -4,5 +4,6 @@ namespace Infrastructure.Interface
     public interface IArticleRepository
     {
         Task AddShoppingListAsync<T>(T entity) where T : class;
+        Task<string> GetFamilyNameAsync(Guid familyId);
     }
 }
