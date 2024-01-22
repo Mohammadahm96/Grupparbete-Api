94d0bb111d248a77a11c8d3509f1a700c33442f4
Mohamad Ahmad <mohamad.ahmad@student.nbi-handelsakademin.se>
2024-01-22 02:59:13 +0100

    Skapat command och commandhandler för update articles

---
+++ Application/Commands/FamilyShoppingListCommands/UpdateFamilyShoppingList/UpdateArticleInFamilyShoppingListCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Models.ListModels;
+using Infrastructure.Interface;
+
+namespace Application.Commands.FamilyShoppingListCommands.UpdateFamilyShoppingList
+{
+    public class UpdateArticleInFamilyShoppingListCommandHandler : IRequestHandler<UpdateArticleInFamilyShoppingListCommand, FamilyArticleList>
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public UpdateArticleInFamilyShoppingListCommandHandler(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+        public async Task<FamilyArticleList> Handle(UpdateArticleInFamilyShoppingListCommand request, CancellationToken cancellationToken)
+        {
+            if (request.UpdateFamilyShoppingListDto.FamilyId == Guid.Empty || request.UpdateFamilyShoppingListDto.ArticleId == Guid.Empty)
+            {
+                throw new Exception("FamilyId and ArticleId are required to update an article in the family shopping list.");
+            }
+
+            FamilyArticleList existingFamilyList = await _articleRepository.GetFamilyArticleAsync(request.UpdateFamilyShoppingListDto.FamilyId, request.UpdateFamilyShoppingListDto.ArticleId);
+
+            if (existingFamilyList == null)
+            {
+                throw new InvalidOperationException($"Article with FamilyId {request.UpdateFamilyShoppingListDto.FamilyId} and ArticleId {request.UpdateFamilyShoppingListDto.ArticleId} not found.");
+            }
+
+            existingFamilyList.ArticleName = request.UpdateFamilyShoppingListDto.ArticleName;
+
+            try
+            {
+                await _articleRepository.UpdateShoppingListAsync(existingFamilyList);
+                return existingFamilyList;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to update FamilyList in the database", ex);
+            }
+        }
+    }
+}
