f18b04d4ce7dc0cdf96c52ee1971b589502ec8c3
Mohamad Ahmad <mohamad.ahmad@student.nbi-handelsakademin.se>
2024-01-31 03:55:35 +0100

    Command and handler for deleting articles

---
+++ Application/Commands/FamilyShoppingListCommands/DeleteFamilyShoppingLists/DeleteArticleFromFamilyShoppingListCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Domain.Models.ListModels;
+using Application.Dto;
+
+namespace Application.Commands.FamilyShoppingListCommands.DeleteFamilyShoppingList
+{
+    public class DeleteArticleFromFamilyShoppingListCommand : IRequest<FamilyArticleList>
+    {
+        public DeleteArticleFromFamilyShoppingListCommand(DeleteArticleFromFamilyShoppingListDto deleteFamilyShoppingListDto)
+        {
+            DeleteFamilyShoppingListDto = deleteFamilyShoppingListDto;
+        }
+
+        public DeleteArticleFromFamilyShoppingListDto DeleteFamilyShoppingListDto { get; }
+    }
+}
---
+++ Application/Commands/FamilyShoppingListCommands/DeleteFamilyShoppingLists/DeleteArticleFromFamilyShoppingListCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Models.ListModels;
+using Infrastructure.Interface;
+
+namespace Application.Commands.FamilyShoppingListCommands.DeleteFamilyShoppingList
+{
+    public class DeleteArticleFromFamilyShoppingListCommandHandler : IRequestHandler<DeleteArticleFromFamilyShoppingListCommand, FamilyArticleList>
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public DeleteArticleFromFamilyShoppingListCommandHandler(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+        public async Task<FamilyArticleList> Handle(DeleteArticleFromFamilyShoppingListCommand request, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(request.DeleteFamilyShoppingListDto.FamilyId) || string.IsNullOrEmpty(request.DeleteFamilyShoppingListDto.ArticleId))
+            {
+                throw new Exception("FamilyId and ArticleId are required to delete an article from the family shopping list.");
+            }
+
+            if (!Guid.TryParse(request.DeleteFamilyShoppingListDto.FamilyId, out var familyId) || !Guid.TryParse(request.DeleteFamilyShoppingListDto.ArticleId, out var articleId))
+            {
+                throw new Exception("Invalid format for FamilyId or ArticleId.");
+            }
+
+            try
+            {
+                await _articleRepository.DeleteFamilyArticleAsync(request.DeleteFamilyShoppingListDto.FamilyId, request.DeleteFamilyShoppingListDto.ArticleId);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to delete FamilyList from the database", ex);
+            }
+        }
+    }
+}
